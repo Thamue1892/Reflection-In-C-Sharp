60f0a506e5bc229a3b62be496e0e41aa490d8dc6
ThaymesCoder <tmpofu@thami.co.za>
2018-02-10 23:30:26 +0200

    Retrieving constructor information from an assembly

--- ReflectionInCSharp/Program.cs
+++ ReflectionInCSharp/Program.cs
@@ -5,6 +5,15 @@ namespace ReflectionInCSharp
 {
     class Program
     {
+
+        public static void PrintMembers(MemberInfo[] ms)
+        {
+            foreach (MemberInfo m in ms)
+            {
+                Console.WriteLine("{0}{1}","    ",m);
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
 
@@ -16,6 +25,24 @@ namespace ReflectionInCSharp
             //Using reflection to get information from an Assembly
             Assembly info = typeof(System.Int32).Assembly;
             Console.WriteLine(info);
+            Console.WriteLine();
+
+            //Getting type objects from a loaded assembly
+            Assembly a = typeof(object).Module.Assembly;
+            Console.WriteLine(a);
+            Console.WriteLine();
+
+            //Listing all the public constructors of the System.String class.
+            Type t = typeof(System.String);
+            Console.WriteLine("Listing all the public constructors of the {0} type",t);
+
+            //Constructors
+            ConstructorInfo[] ci = t.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine("//Constructors");
+
+            PrintMembers(ci);
+
+
         }
     }
 }
